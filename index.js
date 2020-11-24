004e264da37afe3810de4de594931610dbc6e053
cep104 <candicelizabethpeters@gmail.com>
2020-11-24 15:38:43 -0600

    notes

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+  }
+  
+  function nestedTarget() {
+    return document.querySelector('#nested .target');
+  }
+  
+  function increaseRankBy(n) {
+    //   console.log(n) then look in console
+    var ranks = document.querySelectorAll('ul.ranked-list li');
+    for (var i = 0; i < ranks.length; i++) {
+      ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+    //   have to turn the number into an integer to add it to n
+    }
+  }
+  
+//   function deepestChild() {
+     
+//     let node = document.getElementById('grand-node');
+//     let nextNode = node.children[0];
+  
+//     while (nextNode) {
+//       node = nextNode;
+//       nextNode = node.children[0];
+//     }
+  
+//     return node;
+//   }
+
+  function deepestChild() {
+    var node = document.getElementById('grand-node')
+      while (node.children.length > 0) {
+        //   while it has one child then when it's done return what is inside
+        node = node.children[0]
+        console.log(node.children[0])
+      }
+      return node
+    }
+
+    deepestChild()
+
